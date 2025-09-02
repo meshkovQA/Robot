f1482e1feb89acbe397474ca9010cf764ddd5c5c
alekslynx <siriusmc@mail.ru>
2025-09-02 11:48:40 +0200

    v 5.73

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == "__main__":
     # host/port — как у тебя было
-    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True, use_reloader=False)
